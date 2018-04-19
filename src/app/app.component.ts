ec38c3f3bedbf00f60629fae363fab36bb48e72c
Nicolas Bouème <nicolasboueme@gmail.com>
2018-04-19 18:09:24 +0200

    CSS update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Nav, Platform } from "ionic-angular";
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
 import { AngularFireAuth } from "angularfire2/auth";
-import { Observable } from "rxjs/Observable";
 
 import { HomePage } from "../pages/home/home";
 import { LoginPage } from './../pages/login/login';
